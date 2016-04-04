0b50879708b76d99681f502aece625aa319b5d4a
Ionova Katya <katerinaion97@mail.ru>
2016-04-04 14:03:58 +0300

    Релизная версия программы. Signed-off-by: Neogara <Neogara519@gmail.com>

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System;
+using System.Windows.Controls;
+using SpeechLib;
+
+
+namespace WpfApplication1
+{
+    public partial class MainWindow : Window
+
+    {
+        string YandexApiKey = "trnsl.1.1.20160205T131653Z.319382340b2818f8.62e27002c0ae81ba76cdb819441608aa20749438";
+
+        TranslateClass Translate = new TranslateClass(); // Обьект переводчика  
+        SpVoice Voise = new SpVoice() ; // Обьект говорилки 
+      
+
+        string[] LanguageSelectorConvert =
+        {
+            "en",
+            "de",
+            "ru",
+            "zh"
+        };
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InputText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key == Key.Enter) || (e.Key == Key.Tab))
+            {
+                TransliteInterfaseWork();
+            }
+        }
+
+        private void TransliteInterfaseWork()
+        {
+            if ((comboBox.SelectedIndex != -1))
+            {
+                DoubleAnimation Animation = new DoubleAnimation();
+                Animation.To = 0;
+                Animation.Duration = TimeSpan.FromSeconds(0.15);
+                Animation.AutoReverse = true;
+
+                Translate.TranslateText(InputText.Text, LanguageSelectorConvert[comboBox.SelectedIndex],YandexApiKey);
+                OuputText.Text = Translate.text[0]; 
+                OuputText.BeginAnimation(TextBox.WidthProperty, Animation);
+
+                Clipboard.Clear();
+                Clipboard.SetText(OuputText.Text);
+            }
+        }
+
+        private void TranlateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((comboBox.SelectedIndex != -1))
+            {
+                TransliteInterfaseWork();
+            }
+        }
+    
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void InputText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            InputText.SelectAll();
+        }
+
+        private void OuputText_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(OuputText.Text);
+
+        }
+
+       
+
+        private void ShowDopToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (ShowDopToggleButton.IsChecked ?? false)
+
+            {
+                this.Height += 200;
+            }
+            else
+            {
+                this.Height -= 200;
+            }
+        }
+
+        private void Window_Activated(object sender, System.EventArgs e)
+        {
+            InputText.Focus();
+            InputText.SelectAll();
+        }
+
+
+        private void OuputText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            OuputText.SelectAll();
+        }
+
+        private  void SpeakTextButton_Click(object sender, RoutedEventArgs e)
+        {
+             Voise.Speak(OuputText.Text, SpeechVoiceSpeakFlags.SVSFlagsAsync);
+        }
+    }
+}
---
+++ WpfApplication1/YandexTranslateApi.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net;
+using System.Windows;
+
+public class TranslateClass
+{
+
+    public int code { get; set; }
+    public string lang { get; set; }
+    public List<string> text { get; set; }
+    public string ApiKey { get; set; }
+
+    private string strJson;
+    public TranslateClass()
+    {
+        this.text = new List<string>();
+        this.lang = "None";
+        this.code = 0;
+    }
+
+    public  void TranslateText(string word, string language, string ApiKey)
+    {
+        string strUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?";
+        strUrl += "key=" + ApiKey;
+        strUrl += "&text=" + word;
+        strUrl += "&lang=" + language;
+        WebClient wc = new WebClient();
+        wc.Encoding = Encoding.UTF8;
+        try
+        {
+            strJson =  wc.DownloadString(new System.Uri(strUrl));
+            var result = JsonConvert.DeserializeObject<TranslateClass>(strJson);
+            this.text = result.text;
+            this.code = result.code;
+            this.lang = result.lang;
+        }
+        catch (WebException)
+
+        {
+            MessageBox.Show("Something went wrong :(");
+        }
+
+
+    }
+}
+
+
